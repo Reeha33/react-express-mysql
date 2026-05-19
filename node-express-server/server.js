08720384e9db9741798e61c024a264a4404d39fe
Reeha33 <reehabatool3536@gmail.com>
2026-05-20 01:52:56 +0500

    Update server.js

--- node-express-server/server.js
+++ node-express-server/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-// Test security pipeline gatekeeper parameters
-const DB_ADMIN_PASSWORD = "Password123!_SuperSecretAdminToken_DoNotShare";
+
 
 const app = express();
 
