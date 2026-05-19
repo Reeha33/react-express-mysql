f1a36889ccecc3889cfd70ac598a295c954b4bf2
Reeha33 <reehabatool3536@gmail.com>
2026-05-19 14:55:08 +0500

    Update db.config.js

--- node-express-server/app/config/db.config.js
+++ node-express-server/app/config/db.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "123456",
-  DB: "testdb",
-  dialect: "mysql",
+  HOST: "mysql-taskapp-reeha.mysql.database.azure.com", // This is your server building name
+  USER: "dbuser", 
+  PASSWORD: "Devops@123456789", 
+  DB: "flexibleserverdb", // <-- CHANGE THIS BACK TO Azure's default database room!
+  dialect: "mysql", 
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
   pool: {
     max: 5,
     min: 0,
