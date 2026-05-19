0af99bd58c5d6dbb7ceb26100e66c282a4e804fa
Reeha33 <reehabatool3536@gmail.com>
2026-05-19 12:06:57 +0500

    Update http-common.js

--- react-client/src/http-common.js
+++ react-client/src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  // Make sure this points to the BACKEND domain, followed by /api
+  baseURL: "https://app-taskapp-backend-reeha-fnd0b7b9dqcpazh8.eastasia-01.azurewebsites.net/api",
   headers: {
     "Content-type": "application/json"
   }
